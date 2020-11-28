a5bc17c0a6dbf607faf444455c1a51bc03d286f9
amrnumenor <m.amiruddin27@gmail.com>
2020-11-28 16:36:16 +0800

    fix 13-19

--- GNFST.py
+++ GNFST.py
@@ -15,18 +15,14 @@ from nltk.nltk_contrib.fst.fst import *
 class myFST(FST):    
     def recognize(self, iput, oput):
         
-        self.inp = iput.split("-")[::-1]
+        self.inp = iput.split(" ")[::-1] if '-' not in iput else iput.split("-")
         self.outp = oput.split("-")     
 
         print(self.inp)
-        print(self.outp)
 
         transduce_inp = f.transduce(self.inp)
 
-        if self.outp == transduce_inp:        
-            return True
-        else:
-            return False
+        return transduce_inp        
 
 def numberToWords(N):
 
@@ -44,13 +40,13 @@ def numberToWords(N):
             10:'ten', 
             11:'eleven', 
             12:'twelve', 
-            13:'three-ten', 
-            14:'four-ten', 
-            15:'five-ten',
-            16:'six-ten', 
-            17:'seven ten', 
-            18:'eight-ten', 
-            19:'nine-ten'
+            13:'three ten', 
+            14:'four ten', 
+            15:'five ten',
+            16:'six ten', 
+            17:'seven-ten', 
+            18:'eight ten', 
+            19:'nine ten'
             }
     
     tens = [
@@ -65,18 +61,18 @@ def numberToWords(N):
             ]
     
     misc = {
-            100:"one hundred",
-            1000:"one thousand",
-            10000:"ten thousand",
-            100000:"one hundred thousand",
-            10000000: "one million"
+            100:"one-hundred",
+            1000:"one-thousand",
+            10000:"ten-thousand",
+            100000:"one-hundred-thousand",
+            10000000: "one-million"
             }
 
     if 0 <= N <= 19:
         return ones[N]
     elif 20 <= N <= 99:
         Q, R = divmod(N, 10)
-        return tens[Q - 2] + '-and-' + ones[R] if R else tens[Q - 2]
+        return tens[Q - 2] + ' and ' + ones[R] if R else tens[Q - 2]
     elif 100 <= N:
         return misc[N]
     else:
@@ -89,7 +85,7 @@ if __name__ == '__main__':
 
     f = myFST('Finite State Transducer for German Numbers')
     # add state
-    for i in range(1,7):
+    for i in range(1,9):
         f.add_state(str(i))
 
     # initial state
@@ -105,44 +101,52 @@ if __name__ == '__main__':
     f.add_arc('1', '2', ['five'], ['fünf'])
     f.add_arc('1', '2', ['six'], ['sechs'])
     f.add_arc('1', '2', ['seven'], ['sieben'])
+    f.add_arc('1', '4', ['seven'], ['sieb'])
     f.add_arc('1', '2', ['eight'], ['acht'])
     f.add_arc('1', '2', ['nine'], ['neun'])
-    f.add_arc('1', '2', ['ten'], ['zehn'])
-    f.add_arc('2', '2', ['ten'], ['zehn'])
-    f.add_arc('1', '2', ['eleven'], ['elf'])
-    f.add_arc('1', '2', ['twelve'], ['zwolf'])
-    f.add_arc('1', '2', ['seven ten'], ['siebzehn'])
+    f.add_arc('1', '6', ['ten'], ['zehn'])
+    f.add_arc('1', '6', ['eleven'], ['elf'])
+    f.add_arc('1', '6', ['twelve'], ['zwolf'])
 
     # and
-    f.add_arc('3', '4', ['and'], ['und']) # special 'und' for 'ein
-    f.add_arc('2', '4', ['and'], ['und'])
+    f.add_arc('3', '5', ['and'], ['und']) # special 'und' for 'ein
+    f.add_arc('2', '5', ['and'], ['und'])
 
     # tens
-    f.add_arc('4', '5', ['twenty'], ['zwanzig'])
-    f.add_arc('4', '5', ['thirty'], ['dreißig'])
-    f.add_arc('4', '5', ['forty'], ['vierzig'])
-    f.add_arc('4', '5', ['fifty'], ['fünfzig'])
-    f.add_arc('4', '5', ['sixty'], ['sechzig'])
-    f.add_arc('4', '5', ['seventy'], ['siebzig'])
-    f.add_arc('4', '5', ['eighty'], ['achtzig'])
-    f.add_arc('4', '5', ['ninety'], ['neunzig'])
+    f.add_arc('2', '6', ['ten'], ['zehn'])
+    f.add_arc('4', '6', ['ten'], ['zehn']) # special 'zehn' for 'sieb'
+    f.add_arc('5', '6', ['twenty'], ['zwanzig'])
+    f.add_arc('5', '6', ['thirty'], ['dreißig'])
+    f.add_arc('5', '6', ['forty'], ['vierzig'])
+    f.add_arc('5', '6', ['fifty'], ['fünfzig'])
+    f.add_arc('5', '6', ['sixty'], ['sechzig'])
+    f.add_arc('5', '6', ['seventy'], ['siebzig'])
+    f.add_arc('5', '6', ['eighty'], ['achtzig'])
+    f.add_arc('5', '6', ['ninety'], ['neunzig'])
 
     # misc
-    f.add_arc('1', '6', ['one hundred'], ['einhundert'])
-    f.add_arc('1', '6', ['one thousand'], ['eintausend'])
-    f.add_arc('1', '6', ['ten thousand'], 'zehntaunsend')
-    f.add_arc('1', '6', ['one hundred thousand'], ['einhunderttausend'])
-    f.add_arc('1', '6', ['one million'], ['einemillion'])
+    f.add_arc('3', '7', ['hundred'], ['hundert']) # one hundred
+    f.add_arc('2', '7', ['hundred'], ['hundert']) # 2-9 hundred
+    f.add_arc('3', '8', ['thousand'], ['tausend']) # one thousand
+    f.add_arc('2', '8', ['thousand'], ['tausend']) # 2-9 thousand
+    f.add_arc('5', '8', ['thousand'], ['tausend']) # 10-90 thousand
+    f.add_arc('6', '8', ['thousand'], ['tausend']) # hundred thousand
+    f.add_arc('3', '8', ['million'], ['million']) # one million
 
     # accept state
     f.set_final('2')
-    f.set_final('5')
     f.set_final('6')
+    f.set_final('7')
+    f.set_final('8')
 
-    if f.recognize(EN_str, DE_str):
+    transduced = f.recognize(EN_str, DE_str)
+
+    print(transduced)
+
+    if transduced:
         print("accept")
         # input-output mapping
-        print(str(number) + " --> " + DE_str)
+        print(str(number) + " --> " + "-".join(transduced))
     else:
         print("reject")
 
